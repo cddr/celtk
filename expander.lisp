782c1acc21bea457e721bc0077ea6833c9d7a2a1
Andy Chambers <achambers.home@gmail.com>
2008-03-20 19:56:39 +0000

    add right/down arrows to expander button

--- expander.lisp
+++ expander.lisp
@@ -1,37 +1,63 @@
 
 (in-package :celtk)
 
+;;; Creates a pathname with NAME and TYPE in the same
+;;; directory/host/device/whatever as this lisp file. Tries to get
+;;; that at compile time to cope with some useful ASDF extensions that
+;;; place fasls in arbitrary places.
 (defun data-pathname (name type)
   (merge-pathnames (make-pathname :name name :type type)
                    #.(or *compile-file-truename* *load-truename*)))
 
 (defmodel expander (frame-stack)
   ((label :initarg :label :accessor label :initform (c-in nil))
+   (arrows :initarg :arrows :accessor arrows :initform (c-in nil))
    (expansion :initarg :expansion :accessor expansion :initform nil)
    (expanded :initarg :expanded :accessor expanded :initform (c-in nil)))
   (:default-initargs
-    :fm-parent (error "expander widget must have some kind of parent")))
+      ;; if this doesn't work, try replacing the data-pathname
+      ;; form with absolute file locations
+      :arrows (list (list 'right (data-pathname "right" "gif"))
+		    (list 'down (data-pathname "down" "gif")))
+    :expanded (c-in t)))
+
+(defobserver arrows ()
+  (loop for (name filename) in (set-difference new-value old-value :key 'car)
+     do (tk-format `(:pre-make-tk ,self)
+		   "image create photo ~(~a.~a~) -file {~a}"
+		   (^path) name (namestring filename))))
 
 (defmacro mk-expander ((&rest inits) &body body)
   `(make-instance 'expander
-                  ,@inits
-                  :fm-parent *parent*
-                  :expansion (c? (the-kids ,@body))
-                  :expanded (c-in t)
-                  :kids (c? (the-kids
-                             (mk-button-ex ((^label) (setf (expanded (upper self expander))
-							   (not (expanded (upper self expander)))))
-					   :height 10
-					   :width 50)
-			     (^expansion)))
+		  ,@inits
+		  :fm-parent *parent*
+		  :expansion (c? (the-kids ,@body))
 		  :kids-packing (c? (when (^kids)
 				      (if (^expanded)
-					  (format nil "pack~{ ~a~} -side top -anchor nw  -padx ~a -pady ~a"
+					  (format nil "pack~{ ~a~} -side top -anchor nw  -padx ~a -pady ~a -fill x"
 						  (mapcar 'path (^kids))
 						  (^padx) (^pady))
-
-					  (format nil "pack forget~{ ~a~}"
-						  (mapcar 'path (cdr (^kids)))))))))
+					  (with-output-to-string (s)
+					    (format s "pack forget~{ ~a~}~%"
+						    (mapcar 'path (cdr (^kids))))
+					    (format s "pack ~a -side top -anchor nw -padx ~a -pady ~a -fill x"
+						    (path (car (^kids)))
+						    (^padx) (^pady))
+					    s))))
+		  :kids (c? (the-kids
+			     (mk-button-ex ((^label)
+					    (setf (expanded (upper self expander))
+						  (not (expanded (upper self expander)))))
+					   :image (c? (format nil "~(~a.~a~)" 
+							      (path (upper self expander))
+							      (if (expanded (upper self expander))
+								  'down
+								  'right)))
+					   ;; can't use anchor/compound option on ttk button
+					   :tile? nil 
+					   :anchor 'w
+					   :compound 'left)
+			     (^expansion)))))
 
 
 (defmodel expander-test (window)
@@ -39,8 +65,10 @@
   (:default-initargs
       :kids (c? (the-kids
 		 (mk-stack (:packing (c?pack-self))
-		   (mk-expander (:label "hi")
-		     (mk-label :text "hi")
+		   (mk-expander (:label "hi"
+                                 :expanded (c-in nil)
+                                 :width 100)
+		     (mk-label :text "hisldkfj slkdjf sldkf jsldkfjdslkfj")
 		     (mk-label :text "ho")))))))
 
 
