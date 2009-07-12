cd3a4bdcb62fb000b7ddd33e3b5b9eeb11cc1e6a
Andy Chambers <achambers.home@gmail.com>
2009-07-12 02:17:44 +0100

    added expander copyright header

--- expander.lisp
+++ expander.lisp
@@ -1,3 +1,20 @@
+;; -*- mode: Lisp; Syntax: Common-Lisp; Package: cells; -*-
+#|
+
+    Celtk -- Cells, Tcl, and Tk
+
+Copyright (C) 2009 by Andy Chambers
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the Lisp Lesser GNU Public License
+ (http://opensource.franz.com/preamble.html), known as the LLGPL.
+
+This library is distributed  WITHOUT ANY WARRANTY; without even 
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
+See the Lisp Lesser GNU Public License for more details.
+
+|#
 
 (in-package :celtk)
 
