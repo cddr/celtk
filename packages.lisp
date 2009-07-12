7c20254ec860b18957702552b5d48e6d0d819b4d
Andy Chambers <achambers.home@gmail.com>
2009-07-12 19:42:56 +0100

    tidy up defpackage declaration
    Make packages.lisp more diff friendly by putting each exported
    symbol on a new line.

--- packages.lisp
+++ packages.lisp
@@ -4,31 +4,137 @@
   (:nicknames "CTK")
   (:use :common-lisp :utils-kt :cells :cffi)
   (:export
-   #:right #:left
-   #:<1> #:tk-event-type #:xsv #:name #:x #:y #:x-root #:y-root
-   #:title$ #:pop-up #:path #:parent-path #:^keyboard-modifiers
-   #:window #:panedwindow #:mk-row #:c?pack-self #:mk-stack #:mk-text-widget #:text-widget
+   #:right 
+   #:left
+   #:<1> 
+   #:tk-event-type 
+   #:xsv 
+   #:name 
+   #:x 
+   #:y 
+   #:x-root 
+   #:y-root
+   
+   #:title$ 
+   #:pop-up 
+   #:path 
+   #:parent-path 
+   #:^keyboard-modifiers
+   
+   #:window 
+   #:panedwindow 
+   #:mk-row 
+   #:c?pack-self 
+   #:mk-stack 
+   #:mk-text-widget 
+   #:text-widget
+   
    #:mk-panedwindow
-   #:mk-stack #:mk-radiobutton #:mk-radiobutton-ex #:mk-radiobutton #:mk-label
-   #:^selection #:selection #:tk-selector
-   #:mk-checkbutton #:button #:mk-button #:mk-button-ex  #:entry #:mk-entry #:text
-   #:frame-stack #:frame-row #:mk-frame-stack #:path #:^path
-   #:mk-menu-entry-radiobutton #:mk-menu-entry-checkbutton
-   #:mk-menu-radio-group #:mk-menu-entry-separator
-   #:mk-menu-entry-command #:mk-menu-entry-command-ex
-   #:menu #:mk-menu #:^menus #:mk-menu-entry-cascade #:mk-menubar
-   #:^entry-values #:tk-eval #:tk-eval-list #:scale #:mk-scale #:mk-popup-menubutton
-   #:item #:polygon #:mk-polygon #:oval #:mk-oval #:line #:mk-line #:arc #:mk-arc
-   #:text-item #:mk-text-item #:item-geometer
-   #:rectangle #:mk-rectangle #:bitmap #:mk-bitmap #:canvas #:mk-canvas #:mk-frame-row
-   #:mk-scrolled-list #:listbox-item #:mk-spinbox
-   #:mk-scroller #:mk-menu-entry-cascade-ex
-   #:with-ltk #:tk-format #:send-wish #:value #:.tkw
-   #:tk-user-queue-handler #:user-errors #:^user-errors
-   #:timer #:timers #:repeat #:executions #:state #:timer-reset #:make-timer-steps
-   #:^widget-menu #:widget-menu #:tk-format-now
-   #:coords #:^coords #:tk-translate-keysym
-   #:*tkw*))
+   
+   #:mk-stack 
+   #:mk-radiobutton 
+   #:mk-radiobutton-ex 
+   #:mk-radiobutton 
+   #:mk-label
+   
+   #:^selection 
+   #:selection 
+   #:tk-selector
+   
+   #:mk-checkbutton 
+   #:button 
+   #:mk-button 
+   #:mk-button-ex  
+   #:entry 
+   #:mk-entry 
+   #:text
+   
+   #:frame-stack 
+   #:frame-row 
+   #:mk-frame-stack 
+   #:path 
+   #:^path
+   
+   #:mk-menu-entry-radiobutton 
+   #:mk-menu-entry-checkbutton
+   
+   #:mk-menu-radio-group 
+   #:mk-menu-entry-separator
+   
+   #:mk-menu-entry-command 
+   #:mk-menu-entry-command-ex
+   
+   #:menu 
+   #:mk-menu 
+   #:^menus 
+   #:mk-menu-entry-cascade 
+   #:mk-menubar
+   
+   #:^entry-values 
+   #:tk-eval 
+   #:tk-eval-list 
+   #:scale 
+   #:mk-scale 
+   #:mk-popup-menubutton
+   
+   #:item 
+   #:polygon 
+   #:mk-polygon 
+   #:oval 
+   #:mk-oval 
+   #:line 
+   #:mk-line 
+   #:arc 
+   #:mk-arc
+   
+   #:text-item 
+   #:mk-text-item 
+   #:item-geometer
+   
+   #:rectangle 
+   #:mk-rectangle 
+   #:bitmap 
+   #:mk-bitmap 
+   #:canvas 
+   #:mk-canvas 
+   #:mk-frame-row
+   
+   #:mk-scrolled-list 
+   #:listbox-item 
+   #:mk-spinbox
+   
+   #:mk-scroller 
+   #:mk-menu-entry-cascade-ex
+   
+   #:with-ltk 
+   #:tk-format 
+   #:send-wish 
+   #:value 
+   #:.tkw
+   
+   #:tk-user-queue-handler 
+   #:user-errors 
+   #:^user-errors
+   
+   #:timer 
+   #:timers 
+   #:repeat 
+   #:executions 
+   #:state 
+   #:timer-reset 
+   #:make-timer-steps
+   
+   #:^widget-menu 
+   #:widget-menu 
+   #:tk-format-now
+   
+   #:coords 
+   #:^coords 
+   #:tk-translate-keysym
+   
+   #:*tkw*
+
+   ))
 
 
 (defpackage :celtk-user
